b3d5f2bf2fa8560ef44ade54f05ae6a4f45dfd6d
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-19 15:45:51 +0100

    add img and posts

--- app.js
+++ app.js
@@ -1,4 +1,50 @@
-const express = reqire('express');
-const app =express();
-port = 3000;
 
+const express = require('express');
+const { url } = require('inspector');
+const app = express();
+const port = 3000
+app.use(express.static('public'))
+
+app.get('/',(req, res)=> {
+    res.send ("server del mio blog")
+})
+
+app.get('/bacheca',(req, res)=> {
+    const posts =[
+{   
+    titolo: "Ciambellone",
+    contenuto:"",
+    img:"./public/img/ciambellone.jpeg",
+    tags : ["ciambellone", "dolce"]
+},
+{   
+    titolo: "Cracker alla barbabietola",
+    contenuto:"",
+    img:"./public/img/cracker_barbabietola.jpeg",
+    tags : ["snack","salato"]
+},
+{   
+    titolo: "Pane fritto",
+    contenuto:"",
+    img:"./public/img/cracker_barbabietola.jpeg",
+    tags : ["pane", "cina"]
+},
+{   
+    titolo: "Pasta alla barbabietola",
+    contenuto:"",
+    img:"./public/img/pasta_barbabietola.jpeg",
+    tags : ["pasta", "barbabietola", "rosso"]
+},
+{   
+    titolo: "Torta paesana",
+    contenuto:"",
+    img:"./public/img/torta_paesana.jpeg",
+    tags : ["tradizione", "torta", "dolce"]
+}]
+
+})
+
+app.listen(port ,()=>{
+    console.log("il server è online");
+    
+})
